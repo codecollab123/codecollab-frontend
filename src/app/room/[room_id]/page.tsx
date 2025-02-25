953ee7ed842eecbcf2b33ff68678549834433ed2
Isha Upadhyay <isha962002sun@gmail.com>
2025-02-25 12:18:00 +0530

    fiex

--- src/app/room/[room_id]/page.tsx
+++ src/app/room/[room_id]/page.tsx
@@ -136,7 +136,7 @@ export default function CodingRoom() {
             <UserPlus />
             Invite
           </Button>
-          <Link href={"room/whiteboard"}>
+          <Link href={"whiteboard"}>
             <Button>
               <NotebookPen />
               WhiteBoard
