ce99aa71c7334395fc46f97e3d69596d5bb1973a
tushar2708 <rtushar2708@gmail.com>
2025-02-25 21:00:28 +0530

    code collab and user joined

--- src/service/socket.ts
+++ src/service/socket.ts
@@ -7,5 +7,5 @@ export const initSocket = async () => {
     timeout: 10000,
     transports: ['websocket'],
   };
-  return io('http://127.0.0.1:5000/', options);
+  return io(process.env.NEXT_PUBLIC__BASE_URL, options);
 };
